d7e6a6de8f8bed4b7d99330ab42f7f06c506bdc3
Cyber Neko <firexneko7@gmail.com>
2021-10-24 21:01:06 +0900

    Layout作成

--- app/src/main/java/com/example/texttospeechdemo/MainActivity.kt
+++ app/src/main/java/com/example/texttospeechdemo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.texttospeechdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.View.INVISIBLE
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStop.visibility = INVISIBLE
+        btnStop.isEnabled = false
+
     }
 }
