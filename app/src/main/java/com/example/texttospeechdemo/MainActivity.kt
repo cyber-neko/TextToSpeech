d3f77e6d4b392c8e96e79c0cc89d2d5c53578cf0
Cyber Neko <firexneko7@gmail.com>
2021-10-25 09:00:10 +0900

    Android11に対応させるためmanifestファイルを編集

--- app/src/main/java/com/example/texttospeechdemo/MainActivity.kt
+++ app/src/main/java/com/example/texttospeechdemo/MainActivity.kt
@@ -7,6 +7,9 @@ import android.view.View.INVISIBLE
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +17,14 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         btnStop.visibility = INVISIBLE
         btnStop.isEnabled = false
 
+    }
+
+    override fun onInit(status: Int) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+        } else {
+
+        }
     }
 }
